4c2504562d6becafeba2546ae6d944387fae4523
Igor Moskalev <moskalev.igor.v@gmail.com>
2025-02-17 17:09:07 +0500

    feat: used modern approach to access control, add error handling, cache control, proper cleanup configuration, and CloudFront Compression

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -2,9 +2,8 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
-import * as iam from "aws-cdk-lib/aws-iam";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
-// import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 
 export class InfrastructureStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -19,30 +18,35 @@ export class InfrastructureStack extends cdk.Stack {
             websiteIndexDocument: "index.html",
             publicReadAccess: false,
             blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
         });
 
-        // Grant the CloudFront OAI read access to the S3 bucket
-        websiteBucket.addToResourcePolicy(
-            new iam.PolicyStatement({
-                actions: ["s3:GetObject"],
-                principals: [
-                    new iam.CanonicalUserPrincipal(cloudFrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId),
-                ],
-                resources: [websiteBucket.arnForObjects("*")],
-            }),
-        );
-
         // Define a CloudFront distribution with S3 as the origin
-        const distribution = new cloudfront.CloudFrontWebDistribution(this, "WebsiteDistribution", {
-            originConfigs: [
+        const distribution = new cloudfront.Distribution(this, "WebsiteDistribution", {
+            defaultRootObject: "index.html",
+            minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+            errorResponses: [
                 {
-                    s3OriginSource: {
-                        s3BucketSource: websiteBucket,
-                        originAccessIdentity: cloudFrontOAI,
-                    },
-                    behaviors: [{ isDefaultBehavior: true }],
+                    httpStatus: 403,
+                    responseHttpStatus: 200,
+                    responsePagePath: "/index.html",
+                    ttl: cdk.Duration.seconds(30),
+                },
+                {
+                    httpStatus: 404,
+                    responseHttpStatus: 200,
+                    responsePagePath: "/index.html",
+                    ttl: cdk.Duration.seconds(30),
                 },
             ],
+            defaultBehavior: {
+                origin: origins.S3BucketOrigin.withOriginAccessControl(websiteBucket),
+                compress: true,
+                allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+                viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+            },
         });
 
         // Deploy the built React app to the S3 bucket and trigger CloudFront invalidation
